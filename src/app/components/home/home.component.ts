4bc633a466a0e403cff15b6a6f7343754f291fce
copiedcode <raphael.webdesign@outlook.com>
2020-05-30 17:54:43 +0200

    fetch products from db

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from "../../services/product.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -10,12 +11,17 @@ export class HomeComponent implements OnInit {
 
   products: any[] = [];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.productService.getAllProducts().subscribe((prods: {count: Number, products: any[]}) => {
       this.products = prods.products;
     });
+  }
+
+  selectProduct(id: Number){
+    this.router.navigate(['/product', id]).then();
   }
 
 }
