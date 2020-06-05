e10636af7b5fa24eaba6d3dd1b08dff9e2856644
copiedcode <raphael.webdesign@outlook.com>
2020-06-05 14:18:22 +0200

    fixing product errors - need to fix image slider

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,50 +1,59 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, ParamMap, Router} from "@angular/router";
 import {ProductService} from "../../services/product.service";
-import {CartService} from "../../services/cart.service";
-import {ActivatedRoute, ParamMap} from "@angular/router";
+import {ProductModelServer} from "../../models/product.model";
 import {map} from "rxjs/operators";
-
+import {CartService} from "../../services/cart.service";
 
 declare let $: any;
 
 @Component({
-  selector: 'app-product',
+  selector: 'mg-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit, AfterViewInit {
+export class ProductComponent implements AfterViewInit, OnInit {
+
   id: number;
-  product;
-  thumbImages: any[] = [];
+  product: ProductModelServer;
+  thumbimages: any[] = [];
+  image: string;
+
 
   @ViewChild('quantity') quantityInput;
-  constructor(private productService: ProductService,
+
+  constructor(private route: ActivatedRoute,
+              private productService: ProductService,
               private cartService: CartService,
-              private route: ActivatedRoute) { }
+              private router: Router) {
+
+
+  }
 
   ngOnInit(): void {
-    this.route.paramMap
-      .pipe(
-        map((param:ParamMap) => {
-          // @ts-ignore
-          return param.params.id;
-        })
-      )
-      .subscribe(prodID => {
-        this.id = prodID;
-        this.productService.getSingleProduct(this.id).subscribe( prod => {
-          if(status != "error") {
-            this.product = prod;
-          }
+    this.route.paramMap.pipe(
+      map((param: ParamMap) => {
+        // @ts-ignore
+        return param.params.id;
+      })
+    ).subscribe(prodId => {
+      this.id = prodId;
+      this.productService.getSingleProduct(this.id).subscribe((prod: ProductModelServer) => {
+        console.log(prod);
+        if(status != "error") {
+          this.product = prod;
+          this.image = prod.image;
+        }
+        if (prod.images !== null) {
+          this.thumbimages = prod.images.split(';');
+        }
 
-          if(prod.images != null) {
-            this.thumbImages = prod.images.split(';');
-          }
-        })
       });
+    });
   }
 
   ngAfterViewInit(): void {
+
     // Product Main img Slick
     $('#product-main-img').slick({
       infinite: true,
@@ -54,6 +63,7 @@ export class ProductComponent implements OnInit, AfterViewInit {
       fade: true,
       asNavFor: '#product-imgs',
     });
+
     // Product imgs Slick
     $('#product-imgs').slick({
       slidesToShow: 3,
@@ -74,6 +84,7 @@ export class ProductComponent implements OnInit, AfterViewInit {
       },
       ]
     });
+
     // Product img zoom
     var zoomMainProduct = document.getElementById('product-main-img');
     if (zoomMainProduct) {
@@ -81,35 +92,38 @@ export class ProductComponent implements OnInit, AfterViewInit {
     }
   }
 
+  addToCart(id: number) {
+    this.cartService.AddProductToCart(id, this.quantityInput.nativeElement.value);
+  }
+
   Increase() {
     let value = parseInt(this.quantityInput.nativeElement.value);
-
-    if(this.product.quantity >= 1){
+    if (this.product.quantity >= 1){
       value++;
-      if(value > this.product.quantity){
+
+      if (value > this.product.quantity) {
+        // @ts-ignore
         value = this.product.quantity;
       }
     } else {
       return;
     }
+
     this.quantityInput.nativeElement.value = value.toString();
   }
 
   Decrease() {
     let value = parseInt(this.quantityInput.nativeElement.value);
-
-    if(this.product.quantity > 0){
+    if (this.product.quantity > 0){
       value--;
-      if(value <= 1){
-        value = 1;
+
+      if (value <= 0) {
+        // @ts-ignore
+        value = 0;
       }
     } else {
       return;
     }
     this.quantityInput.nativeElement.value = value.toString();
-  }
-
-  addToCart(id: number){
-    this.cartService.AddProductToCart(id, this.quantityInput.nativeElement.value);
   }
 }
