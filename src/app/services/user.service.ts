6b72a1187ee219284b1b743baeeae00f8c832e85
copiedcode <raphael.webdesign@outlook.com>
2020-06-08 11:26:14 +0200

    finish user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import {AuthService, GoogleLoginProvider, SocialUser} from "angularx-social-login";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  auth = false;
+  private SERVER_URL = environment.SERVER_URL;
+  private user;
+  authState$ = new BehaviorSubject<boolean>(this.auth);
+  userData$ = new BehaviorSubject<SocialUser | ResponseModel>(null);
 
-  constructor() { }
+  constructor(private authService: AuthService,
+              private httpClient: HttpClient) {
+
+    authService.authState.subscribe((user:SocialUser) => {
+      if( user != null ) {
+       this.auth = true;
+       this.authState$.next(this.auth);
+       this.userData$.next(user);
+      }
+    });
+  }
+
+  // LOGIN USER (email and password)
+  loginUser(email: string, password: string){
+    this.httpClient.post(`${this.SERVER_URL}/auth/login`, {email, password})
+      .subscribe((data: ResponseModel) => {
+        this.auth = data.auth;
+        this.authState$.next(this.auth);
+        this.userData$.next(data);
+      });
+  }
+
+  // GOOGLE AUTH
+  googleLogin(){
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+
+
+}
+
+interface ResponseModel {
+  token: string;
+  auth: boolean;
+  email: string;
+  username: string;
+  fname:string;
+  lname: string;
+  photoURL: string;
+  userID: number;
+
 }
