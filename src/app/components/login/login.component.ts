36bfabef839ead93f7b8a548e335350a9454a806
copiedcode <raphael.webdesign@outlook.com>
2020-06-08 14:06:50 +0200

    creating login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email: string;
+  password: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  signInWithGoogle() {
+
+  }
+
+  login(form: NgForm) {
+
+  }
 }
